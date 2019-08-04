78e1f93bbfb3ab9b83293840f67548d30203ca99
straengel <straengel@gmail.com>
2019-08-05 01:53:35 +0300

    Готовое 20 задание
    Ток с ие беда, не знаю, будет ли работать

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 'use strict';
+
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+import elemntClosest from 'element-closest';
 import countTimer from "./modules/countTimer";
 import calc from "./modules/calc";
 import changeImg from "./modules/changeImg";
@@ -8,6 +15,7 @@ import tabs from "./modules/tabs";
 import toggleMenu from "./modules/toggleMenu";
 import togglePopUp from "./modules/togglePopUp";
 
+elemntClosest(window);
 //таймер
 countTimer('26 july 2019');
 
