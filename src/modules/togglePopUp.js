ca64c0b85b34c87aa77da49c0feb649d6af1e109
Sergey <straengel@gmail.com>
2019-08-05 13:05:28 +0300

    Исправлена анимация

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,4 +1,5 @@
 //popup
+/*
 const togglePopUp = () => {
     const   popUp = document.querySelector('.popup'),
             popUpBtn = document.querySelectorAll('.popup-btn');
@@ -18,5 +19,40 @@ const togglePopUp = () => {
             }
         }
     });
+};
+/**/
+//popup
+const togglePopUp = () => {
+    const   popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = popup.querySelector('.popup-content'),
+            popupOffsetTop = popupContent.offsetTop,
+            popupAnimate = () => {
+                if(document.documentElement.offsetWidth > 992){
+                    
+                    popup.style.display = 'block';
+                    countTopPopup++;
+                    popupContent.style.top = `${countTopPopup}px`;
+                    if((window.innerHeight / 10) >= countTopPopup){
+                        setTimeout(popupAnimate, 10);
+                    }
+                } else {
+                    popupContent.style.top = `${popupOffsetTop}px`;
+                    popup.style.display = 'block';
+                }
+            };
+    let countTopPopup = 0;
+    //обнуляем высоту
+    popupContent.style.top = 0;
+
+    popupBtn.forEach((elem) => elem.addEventListener('click', popupAnimate)); 
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+        if(document.documentElement.offsetWidth > 992){
+            popupContent.style.top = `0px`;
+            countTopPopup = 0;
+        }
+    }); 
 };
 export default togglePopUp;
